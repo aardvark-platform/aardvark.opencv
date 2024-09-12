98955a94b1f400806f188b731514ad7da97765c2
Martin <mautner@vrvis.at>
2024-09-12 12:18:39 +0200

    Implement image remapping

--- src/Aardvark.OpenCV.Tests/Tests/ImageTests.fs
+++ src/Aardvark.OpenCV.Tests/Tests/ImageTests.fs
@@ -1,5 +1,6 @@
 ﻿namespace Aardvark.OpenCV.Tests
 
+open System
 open Aardvark.Base
 open NUnit.Framework
 open FsUnit
@@ -10,12 +11,34 @@ module ``Image Processing Tests`` =
 
     module private PixImage =
 
-        let random (size : V2i) =
-            let pi = PixImage<float32>(Col.Format.RGBA, size)
+        let private randomValues : Type -> (unit -> obj) =
+            LookupTable.lookup [
+                typeof<float32>, rnd.UniformFloat >> box
+                typeof<uint16>,  rnd.UniformUInt >> uint16 >> box
+                typeof<int32>,   rnd.UniformInt >> box
+            ]
+
+        let private getRandomValue<'T>() =
+            let get = typeof<'T> |> randomValues
+            get() |> unbox<'T>
+
+        let random<'T> (format: Col.Format) (size : V2i) =
+            let pi = PixImage<'T>(format, size)
             for c in pi.ChannelArray do
-                c.SetByIndex(ignore >> rnd.UniformFloat) |> ignore
+                c.SetByIndex(ignore >> getRandomValue<'T>) |> ignore
 
             pi
+
+        let generate<'T> (format: Col.Format) (sub: bool) =
+            let size = V2i(512) + rnd.UniformV2i(1024)
+            let pi = random<'T> format size
+            if sub then
+                let min = 10
+                let offset = rnd.UniformV2i(size - min - 1)
+                let size = min + rnd.UniformV2i(size - offset - min - 1)
+                new PixImage<'T>(pi.Format, pi.Volume.SubVolume(offset.XYO, V3i(size, pi.ChannelCount)))
+            else
+                pi
 
     [<DatapointSource>]
     let filters = [|
@@ -25,27 +48,93 @@ module ``Image Processing Tests`` =
         ImageInterpolation.Lanczos
     |]
 
+    [<DatapointSource>]
+    let borderTypes = [|
+        ImageBorderType.Const
+        ImageBorderType.Repl
+        ImageBorderType.Wrap
+        ImageBorderType.Mirror
+    |]
+
     [<SetUp>]
     let init() =
         Aardvark.Init()
 
     [<Theory>]
     let ``Scaling`` (filter: ImageInterpolation) (sub: bool) =
-        let size = V2i(512) + rnd.UniformV2i(1024)
+        let src = PixImage.generate<float32> Col.Format.RGBA sub
         let scaleFactor = V2d(0.2345, 1.6789)
-
-        let src =
-            let pi = PixImage.random size
-            if sub then
-                let min = 10
-                let offset = rnd.UniformV2i(size - min - 1)
-                let size = min + rnd.UniformV2i(size - offset - min - 1)
-                new PixImage<float32>(pi.Format, pi.Volume.SubVolume(offset.XYO, V3i(size, pi.ChannelCount)))
-            else
-                pi
 
         let result = Aardvark.OpenCV.PixProcessor.Instance.Scale(src, scaleFactor, filter)
         let reference = Aardvark.Base.PixProcessor.Instance.Scale(src, scaleFactor, filter)
 
         let psnr = PixImage.peakSignalToNoiseRatio result reference
         psnr |> should be (greaterThan 20.0)
+
+    [<Theory>]
+    let ``Remapping`` (sub: bool) =
+        let src = PixImage.generate<float32> Col.Format.RGBA sub
+
+        let result =
+            let mapX = Matrix<float32>(src.Size)
+            mapX.SetByCoord(fun x _ -> float32 (x * 2L)) |> ignore
+
+            let mapY = Matrix<float32>(src.Size)
+            mapY.SetByCoord(fun _ y -> float32 (src.SizeL.Y - y - 1L)) |> ignore
+
+            Aardvark.OpenCV.PixProcessor.Instance.Remap(src, mapX, mapY, ImageInterpolation.Near)
+
+        let reference =
+            let pi = src.Transformed(ImageTrafo.MirrorY)
+
+            pi.Volume.SetByCoord(fun x y c ->
+                if x * 2L < pi.SizeL.X then
+                    pi.Volume.[x * 2L, y, c]
+                else
+                    0.0f
+            ) |> ignore
+
+            pi
+
+        let psnr = PixImage.peakSignalToNoiseRatio result reference
+        psnr |> should be (greaterThan 20.0)
+
+    [<Theory>]
+    let ``Remapping (border type)`` (borderType: ImageBorderType) (sub: bool) =
+        let src = PixImage.generate<uint16> Col.Format.BGRA sub
+        let border = [| 1us; 2us; 3us; 4us |]
+
+        let result =
+            let mapX = Matrix<float32>(src.Size)
+            let mapY = Matrix<float32>(src.Size)
+
+            if borderType = ImageBorderType.Const then
+                mapX.Set -1.0f |> ignore
+            else
+                mapX.SetByCoord(fun x _ -> float32 (src.SizeL.X + x)) |> ignore
+                mapY.SetByCoord(fun _ y -> float32 y) |> ignore
+
+            Aardvark.OpenCV.PixProcessor.Instance.Remap(src, mapX, mapY, ImageInterpolation.Near, borderType, border)
+
+        let expected =
+            match borderType with
+            | ImageBorderType.Const ->
+                let pi = new PixImage<uint16>(result.Format, result.Size)
+                pi.GetMatrix<C4us>().Set(C4us border) |> ignore
+                pi
+
+            | ImageBorderType.Repl ->
+                let pi = new PixImage<uint16>(result.Format, result.Size)
+                pi.Volume.SetByCoord(fun _ y c -> src.Volume.[src.Size.X - 1, y, c]) |> ignore
+                pi
+
+            | ImageBorderType.Wrap ->
+                src
+
+            | ImageBorderType.Mirror ->
+                src.Transformed(ImageTrafo.MirrorX)
+
+            | _ -> raise <| NotImplementedException()
+
+        let psnr = PixImage.peakSignalToNoiseRatio result expected
+        psnr |> should equal infinity
--- src/Aardvark.OpenCV/ImageProcessing.cs
+++ src/Aardvark.OpenCV/ImageProcessing.cs
@@ -7,6 +7,8 @@ using CvMat = OpenCvSharp.Mat;
 
 namespace Aardvark.OpenCV
 {
+    #region Utilities
+
     internal readonly struct GCHandleDiposable : IDisposable
     {
         public GCHandle Handle { get; }
@@ -40,32 +42,62 @@ namespace Aardvark.OpenCV
             else throw new NotSupportedException($"Channel type {type} is not supported.");
         }
 
-        private static readonly Dictionary<ImageInterpolation, InterpolationFlags> interpolationFlags = new()
-        {
-            { ImageInterpolation.Near,    (InterpolationFlags)6 }, // INTER_NEAREST_EXACT
-            { ImageInterpolation.Linear,  InterpolationFlags.Linear },
-            { ImageInterpolation.Cubic,   InterpolationFlags.Cubic },
-            { ImageInterpolation.Lanczos, InterpolationFlags.Lanczos4 },
-        };
+        public static InterpolationFlags ToInterpolationFlags(this ImageInterpolation interpolation, bool exact)
+            => interpolation switch
+            {
+                ImageInterpolation.Near when exact   => (InterpolationFlags)6,
+                ImageInterpolation.Near              => InterpolationFlags.Nearest,
+                ImageInterpolation.Linear when exact => InterpolationFlags.LinearExact,
+                ImageInterpolation.Linear            => InterpolationFlags.Linear,
+                ImageInterpolation.Cubic             => InterpolationFlags.Cubic,
+                ImageInterpolation.Lanczos           => InterpolationFlags.Lanczos4,
+                _ => throw new NotSupportedException($"Filter {interpolation} is not supported.")
+            };
 
-        public static InterpolationFlags ToInterpolationFlags(this ImageInterpolation interpolation)
+        public static BorderTypes ToBorderTypes(this ImageBorderType borderType)
+            => borderType switch
+            {
+                ImageBorderType.Const  => BorderTypes.Constant,
+                ImageBorderType.Repl   => BorderTypes.Replicate,
+                ImageBorderType.Wrap   => BorderTypes.Wrap,
+                ImageBorderType.Mirror => BorderTypes.Reflect,
+                _ => throw new NotSupportedException($"Border type {borderType} is not supported.")
+            };
+
+        private static GCHandleDiposable ToMat(this Array data, V2l size, V2l delta, long firstIndex, MatType matType, int elementSize, out CvMat result)
         {
-            if (interpolationFlags.TryGetValue(interpolation, out InterpolationFlags flags)) return flags;
-            else throw new NotSupportedException($"Filter {interpolation} is not supported.");
+            var gc = data.Pin();
+
+            var ptr = IntPtr.Add(gc.AddrOfPinnedObject(), (int)firstIndex * elementSize);
+            var sizei = size.ToV2i();
+            var deltai = delta.YX * elementSize;
+
+            result = CvMat.FromPixelData(sizei.YX.ToArray(), matType, ptr, deltai.ToArray());
+            return gc;
         }
+
+        public static GCHandleDiposable ToMat<T>(this Matrix<T> matrix, MatType matType, int elementSize, out CvMat result)
+            => matrix.Data.ToMat(matrix.Size, matrix.Delta, matrix.FirstIndex, matType, elementSize, out result);
+
+        public static GCHandleDiposable ToMat<T>(this Volume<T> volume, MatType matType, int elementSize, out CvMat result)
+            => volume.Data.ToMat(volume.Size.XY, volume.Delta.XY, volume.FirstIndex, matType, elementSize, out result);
     }
+
+    #endregion
 
     public sealed class PixProcessor : IPixProcessor
     {
         public string Name => "OpenCV";
 
-        public PixProcessorCaps Capabilities => PixProcessorCaps.Scale;
+        public PixProcessorCaps Capabilities => PixProcessorCaps.Scale | PixProcessorCaps.Remap;
 
         [OnAardvarkInit]
         public static void Init()
         {
             PixImage.AddProcessor(Instance);
         }
+
+        #region Scale
 
         public PixImage<T> Scale<T>(PixImage<T> image, V2d scaleFactor, ImageInterpolation interpolation)
         {
@@ -84,34 +116,88 @@ namespace Aardvark.OpenCV
 
             var dst = newSize.CreateImageVolume<T>();
 
-            var matType = typeof(T).ToMatType((int)src.SZ);
+            var matType = typeof(T).ToMatType(image.ChannelCount);
             var elementSize = typeof(T).GetCLRSize();
 
-            using var srcGC = src.Data.Pin();
-            var srcPtr = IntPtr.Add(srcGC.AddrOfPinnedObject(), (int)src.FirstIndex * elementSize);
-            var srcSize = src.Size.XY.ToV2i();
-            var srcDelta = src.Delta.YX * elementSize;
+            using var _src = src.ToMat(matType, elementSize, out var srcMat);
+            using var _dst = dst.ToMat(matType, elementSize, out var dstMat);
 
-            using var dstGC = dst.Data.Pin();
-            var dstPtr = dstGC.AddrOfPinnedObject();
-            var dstSize = dst.Size.XY.ToV2i();
-
-            var srcMat = CvMat.FromPixelData(srcSize.YX.ToArray(), matType, srcPtr, srcDelta.ToArray());
-            var dstMat = CvMat.FromPixelData(dstSize.Y, dstSize.X, matType, dstPtr);
-            Cv2.Resize(srcMat, dstMat, new Size(dstSize.X, dstSize.Y), interpolation: interpolation.ToInterpolationFlags());
+            Cv2.Resize(
+                srcMat, dstMat,
+                new Size((int)dst.SX, (int)dst.SY),
+                interpolation: interpolation.ToInterpolationFlags(true)
+            );
 
             return new (image.Format, dst);
         }
+
+        #endregion
+
+        #region Rotate
 
         public PixImage<T> Rotate<T>(PixImage<T> image, double angleInRadians, bool resize, ImageInterpolation interpolation,
                                      ImageBorderType borderType = ImageBorderType.Const,
                                      T border = default)
             => null;
 
+        #endregion
+
+        #region Remap
+
+        // TODO: Change the interface to accept T[] as border value
+        /// <inheritdoc cref="Remap{T}(PixImage{T}, Matrix{float}, Matrix{float}, ImageInterpolation, ImageBorderType, T)"/>
         public PixImage<T> Remap<T>(PixImage<T> image, Matrix<float> mapX, Matrix<float> mapY, ImageInterpolation interpolation,
                                     ImageBorderType borderType = ImageBorderType.Const,
-                                    T border = default)
-            => null;
+                                    T[] border = default)
+        {
+            var src = image.Volume;
+
+            if (!src.HasImageWindowLayout())
+            {
+                throw new ArgumentException($"Volume must be in image layout (Delta = {src.Delta}).");
+            }
+
+            if (mapX.Size != mapY.Size)
+            {
+                throw new ArgumentException($"Size of coordinate maps must match (mapX: {mapX.Size}, mapY: {mapY.Size}).");
+            }
+
+            var matType = typeof(T).ToMatType(image.ChannelCount);
+            var elementSize = typeof(T).GetCLRSize();
+
+            var dstSize = new V3l(mapX.Size, image.ChannelCountL);
+            var dst = dstSize.CreateImageVolume<T>();
+
+            using var _s = src.ToMat(matType, elementSize, out var srcMat);
+            using var _d = dst.ToMat(matType, elementSize, out var dstMat);
+            using var _x = mapX.ToMat(MatType.CV_32FC1, 4, out var mapXMat);
+            using var _y = mapY.ToMat(MatType.CV_32FC1, 4, out var mapYMat);
+
+            Scalar borderValue = new();
+            if (borderType == ImageBorderType.Const && border != null)
+            {
+                var order = image.Format.ChannelOrder();
+
+                for (var i = 0; i < Fun.Min(border.Length, 4); i++)
+                    borderValue[i] = Convert.ToDouble(border[order[i]]);
+            }
+
+            Cv2.Remap(
+                srcMat, dstMat, mapXMat, mapYMat,
+                interpolation.ToInterpolationFlags(false),
+                borderType.ToBorderTypes(),
+                borderValue
+            );
+
+            return new (image.Format, dst);
+        }
+
+        public PixImage<T> Remap<T>(PixImage<T> image, Matrix<float> mapX, Matrix<float> mapY, ImageInterpolation interpolation,
+                                    ImageBorderType borderType,
+                                    T border)
+            => Remap(image, mapX, mapY, interpolation, borderType, new T[] { border, border, border, border });
+
+        #endregion
 
         private PixProcessor() { }
 
